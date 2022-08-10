597784f46a31859e75ec48b5c9503f4eb0d98f23
danpoggetti <danpoggetti@gmail.com>
2022-08-10 16:08:21 -0700

    finalized highscores js file

--- assets/highscore.js
+++ assets/highscore.js
@@ -2,3 +2,26 @@ var highScore = document.querySelector("#HighScores");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 var finalScore = document.querySelector("#finalScore")
+
+// Here's an eventListener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Adds highscore to local storage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(localStorage.getItem("allScores"));
+console.log("all scores: ", allScores)
+
+if (allScores !== null) {
+    console.log("I am Here")
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+// Here's the eventListner to go back to index.html
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
