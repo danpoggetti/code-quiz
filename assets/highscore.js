9e2b2ab1699e08a063f58c4cf68e4d1d041fd10a
danpoggetti <danpoggetti@gmail.com>
2022-08-10 16:00:12 -0700

    created the highscore.js file

---
+++ assets/highscore.js
@@ -0,0 +1,4 @@
+var highScore = document.querySelector("#HighScores");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+var finalScore = document.querySelector("#finalScore")
